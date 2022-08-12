d273cfef311617efd83dcf68ded99aba650c2984
Clayton2712 <clayton27121996@gmail.com>
2022-08-12 13:24:08 +0200

    initial commit

--- src/main/java/com/clayton/SpringSecurityDemo/controller/AuthController.java
+++ src/main/java/com/clayton/SpringSecurityDemo/controller/AuthController.java
@@ -3,13 +3,20 @@ package com.clayton.SpringSecurityDemo.controller;
 import com.clayton.SpringSecurityDemo.model.ERole;
 import com.clayton.SpringSecurityDemo.model.Role;
 import com.clayton.SpringSecurityDemo.model.User;
+import com.clayton.SpringSecurityDemo.payload.request.LoginRequest;
 import com.clayton.SpringSecurityDemo.payload.request.SignupRequest;
+import com.clayton.SpringSecurityDemo.payload.response.JwtResponse;
 import com.clayton.SpringSecurityDemo.payload.response.MessageResponse;
 import com.clayton.SpringSecurityDemo.repository.RoleRepo;
 import com.clayton.SpringSecurityDemo.repository.UserRepo;
+import com.clayton.SpringSecurityDemo.security.jwt.JwtUtils;
+import com.clayton.SpringSecurityDemo.security.services.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,14 +25,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
 
     @Autowired
-    AuthenticationManager authenticationManagerManager;
+    JwtUtils JwtUtils;
+    @Autowired
+    AuthenticationManager authenticationManager;
 
     @Autowired
     UserRepo userRepo;
@@ -47,7 +58,7 @@ public class AuthController {
         // Create new user's account
         User user = new User(signupRequest.getUsername(),
                 signupRequest.getEmail(),
-                passwordEncoder.encode(signupRequest.getUsername()));
+                passwordEncoder.encode(signupRequest.getPassword()));
         Set<String> strRoles = signupRequest.getRoles();
         Set<Role> roles = new HashSet<>();
         if (strRoles == null) {
@@ -82,6 +93,24 @@ public class AuthController {
         userRepo.save(user);
         return ResponseEntity.ok( new MessageResponse("User registered successfully"));
     }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = JwtUtils.generateJwtToken(authentication);
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
+    }
+
 }
 
 
